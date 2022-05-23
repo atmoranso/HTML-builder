6aab56c420f22235a57d1c107e8099681a07c448
Atmoranso <atmoranso@gmail.com>
2022-05-23 20:35:52 +0300

    feat: add task 3 solution

---
+++ 03-files-in-folder/index.js
@@ -0,0 +1,19 @@
+const { readdir } = require('fs/promises');
+const { stat } = require('fs');
+
+const path = require('path');
+async function getFiles() {
+    try {
+        const files = await readdir(path.join(__dirname, 'secret-folder'));
+        for (const file of files) {
+            stat(path.join(__dirname, 'secret-folder', file), (err, stats) => {
+                if (!stats.isDirectory()) {
+                    console.log(file + ' - ' + path.extname(file).slice(1) + ' - ' + stats.size + ' byte');
+                }
+            });
+        }
+    } catch (err) {
+        console.error(err);
+    }
+}
+getFiles();
